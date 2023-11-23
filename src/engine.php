2f4533053f46f88b6d4665d10390986fe0ff6864
Konstantin <konst.kazan@gmail.com>
2023-11-24 00:56:05 +0300

    add game brain-even

---
+++ src/engine.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Php\Project\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function greetings()
+{
+    global $name;
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
+function tutorial($description)
+{
+    line($description);
+}
+
+function round($question)
+{
+    line("Question: $question");
+    $answer = prompt('Your answer: ');
+    return $answer;
+}
+
+function correct()
+{
+    line('Correct!');
+}
+
+function congratulations()
+{
+    global $name;
+    line("Congratulations, {$name}!");
+}
